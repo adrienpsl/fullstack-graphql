2e45ff40e98936493bc89c632fbe060629dc3df4
adrien <adrien.pusel@gmail.com>
2020-01-03 15:31:08 +0100

    - add start data in db.json - create the apollo starter in client.js - update the Pets stuff to get the data from my component

--- client/src/client.js
+++ client/src/client.js
@@ -1,8 +1,28 @@
-import { ApolloClient } from 'apollo-client'
-import { InMemoryCache } from 'apollo-cache-inmemory'
-import { HttpLink } from 'apollo-link-http'
-import gql from 'graphql-tag'
+import { ApolloClient }  from "apollo-client";
+import { InMemoryCache } from "apollo-cache-inmemory";
+import { HttpLink }      from "apollo-link-http";
 
 /**
  * Create a new apollo client and export as default
+ * we create a link, that is an interface to interact with graphQl sever
  */
+	// I can have multiple client, and multiple link
+const link = new HttpLink( { uri: "http://localhost:4000/" } );
+// read how that's work, because I didn't get all that shit
+const cache = new InMemoryCache( {} );
+
+const client = new ApolloClient( { link, cache } );
+
+// the bracket says : It's a short method
+// const query = gql`
+//     {
+//         characters {
+//             results {
+//                 fullname: name
+//                 id
+//             }
+//         }
+//     }
+// `;
+
+export default client;
--- client/src/pages/Pets.js
+++ client/src/pages/Pets.js
@@ -1,39 +1,62 @@
-import React, {useState} from 'react'
-import gql from 'graphql-tag'
-import { useQuery, useMutation } from '@apollo/react-hooks'
-import PetsList from '../components/PetsList'
-import NewPetModal from '../components/NewPetModal'
-import Loader from '../components/Loader'
+import React, { useState }       from "react";
+import gql                       from "graphql-tag";
+import { useMutation, useQuery } from "@apollo/react-hooks";
+import PetsList                  from "../components/PetsList";
+import NewPetModal               from "../components/NewPetModal";
+import Loader                    from "../components/Loader";
 
+const ALL_PETS = gql`
+    query Name { pets { id name type img } }
+`;
 
-export default function Pets () {
-  const [modal, setModal] = useState(false)
+const NEW_PET = gql`
+    mutation newPet($newPet: NewPetInput! ) {
+        addPet(input: $newPet) { id name type img }
+    }
+`;
 
+export default function Pets() {
+	// the component will be rerender each time the
+	// the hooks will change
+	const [ modal, setModal ] = useState( false );
+	const { data, loading, error } = useQuery( ALL_PETS );
+	const [ createPet, newPet ] = useMutation( NEW_PET );
 
-  const onSubmit = input => {
-    setModal(false)
-  }
-  
-  if (modal) {
-    return <NewPetModal onSubmit={onSubmit} onCancel={() => setModal(false)} />
-  }
+	if ( loading || newPet.loading) {
+		return <Loader></Loader>;
+	}
 
-  return (
-    <div className="page pets-page">
-      <section>
-        <div className="row betwee-xs middle-xs">
-          <div className="col-xs-10">
-            <h1>Pets</h1>
-          </div>
+	if ( error || newPet.error) {
+		return <p>Error</p>;
+	}
 
-          <div className="col-xs-2">
-            <button onClick={() => setModal(true)}>new pet</button>
-          </div>
-        </div>
-      </section>
-      <section>
-        <PetsList />
-      </section>
-    </div>
-  )
+	const onSubmit = async newPet => {
+		await createPet( { variables: { newPet } } );
+		setModal( false );
+	};
+
+	if ( modal ) {
+		return <NewPetModal onSubmit={ onSubmit }
+							onCancel={ () => setModal( false ) }/>;
+	}
+
+	return (
+		<div className="page pets-page">
+			<section>
+				<div className="row betwee-xs middle-xs">
+					<div className="col-xs-10">
+						<h1>Pets</h1>
+					</div>
+
+					<div className="col-xs-2">
+						<button onClick={ () => setModal( true ) }>new pet
+						</button>
+					</div>
+				</div>
+			</section>
+			<section>
+				<PetsList pets={ data.pets }/>
+			</section>
+		</div>
+	);
 }
