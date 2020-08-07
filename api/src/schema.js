8d7144fa6abb6af7d25f45383a9f55c065a085ff
adrien <adrien.pusel@gmail.com>
2020-08-07 02:11:57 +0200

    end first exercice

--- api/src/schema.js
+++ api/src/schema.js
@@ -1,11 +1,26 @@
-const { gql } = require('apollo-server')
+const { gql } = require( 'apollo-server' );
 
 /**
  * Type Definitions for our Schema using the SDL.
  */
 const typeDefs = gql`
-  
+	type User {
+		id: ID!
+		username: String!
+	}
 
+	type Pet {
+		id: ID!
+		createdAt: String!
+		name: String!
+		type: String!
+	}
+
+	type Query {
+		user: User!
+		pets: [Pet]!
+		pet: Pet
+	}
 `;
 
-module.exports = typeDefs
+module.exports = typeDefs;
